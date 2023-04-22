e21b98ad1036590f2516a5f29b9f708bb1eefe09
yuya-iwata <chanpon0339@gmail.com>
2023-04-23 01:15:30 +0900

    add scripts BattleManager and UnitManager

---
+++ TurnBattleGame/Assets/Scripts/BattleManager.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//Manage Battle
+//Player vs Enemy
+public class BattleManager : MonoBehaviour
+{
+    //Get Player
+    //Get Enemy
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    //Attack Player to Enemy
+}
